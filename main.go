cd1c441d1daac975dc5dec30d74ff91fac40293d
gwarzecha <gmwarzecha@tutanota.com>
2024-08-27 13:14:46 -0700

    Update main func to handle URL(s) entered as command line input

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	arg := os.Args[1:]
+
+	if len(arg) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(arg) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	if len(arg) == 1 {
+		BASE_URL := arg[0]
+		fmt.Printf("starting crawl of: %v", BASE_URL)
+	}
 }
